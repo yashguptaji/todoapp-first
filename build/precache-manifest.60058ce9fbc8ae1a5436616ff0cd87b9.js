73b4111d3a5c8395c1f3344b98e5ef3183fecd0d
yashguptaji <yashguptaji2001@gmail.com>
2021-12-15 21:37:25 +0530

    first commit

---
+++ build/precache-manifest.60058ce9fbc8ae1a5436616ff0cd87b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d950e639a694f3900a4f4e6032ee6ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9dd52cd45836f5b45fc",
+    "url": "/static/css/main.855a11a9.chunk.css"
+  },
+  {
+    "revision": "f6c8ee5051c24324785d",
+    "url": "/static/js/2.d2944169.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d2944169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dd52cd45836f5b45fc",
+    "url": "/static/js/main.9e717dc4.chunk.js"
+  },
+  {
+    "revision": "fbaab9a582e8e68a6c8c",
+    "url": "/static/js/runtime-main.2ae115fb.js"
+  }
+]);
